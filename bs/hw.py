572c3b9b9bb49df34f0743b47dd659aa4a063155
Doge12132009 <36975309+Doge12132009@users.noreply.github.com>
2020-05-16 17:57:44 +0800

    Add files via upload

---
+++ bs/hw.py
@@ -0,0 +1,52 @@
+import json
+import random,os,re
+
+inputfile=open("data.json",encoding="UTF-8")
+bs=json.load(inputfile)
+
+famous=bs["famous"]
+after=bs["after"]
+before=bs["before"]
+bullshit=bs["bullshit"]
+
+xxx=input("主題:")
+
+dup=2
+
+def hash(List):
+    global dup
+    pool = list(List) * dup
+    while True:
+        random.shuffle(pool)
+        for elem in pool:
+            yield elem
+
+nextFam=hash(famous)
+nextShit=hash(bullshit)
+
+def newWords():
+    global nextFam
+    xxx = next(nextFam)
+    xxx = xxx.replace(  "a",random.choice(before)) 
+    xxx = xxx.replace(  "b",random.choice(after) )
+    return xxx
+
+def newPar():
+    xxx = ". "
+    xxx += "\r\n"
+    xxx += "    "
+    return xxx
+
+if __name__ == "__main__":
+    for x in xxx:
+        tmp = str()
+        while ( len(tmp) < 1000 ) :
+            node = random.randint(0,100)
+            if node < 5:
+                tmp += newPar()
+            elif node < 20 :
+                tmp += newWords()
+            else:
+                tmp += next(nextShit)
+        tmp = tmp.replace("x",xxx)
+        print(tmp)
